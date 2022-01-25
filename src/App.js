5ecd313aa2ba391d392620d59ee5a76fc434eb4f
Anthony Forte <developer@anthonyforte.dev>
2022-01-25 17:35:31 -0500

    added login gui and adjusted project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ErrorPage from 'src/pages/error-page/ErrorPage';
+import LoginPage from 'src/pages/login-page/LoginPage';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='*' element={<ErrorPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+
 }
-
-export default App;
---
+++ src/pages/error-page/ErrorPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './ErrorPage.css';
+
+export default class ErrorPage extends Component {
+
+  render() {
+    return (
+      <div className='error-404-card'>
+        <h3>404 Page Not Found!</h3>
+      </div>
+    );
+  }
+
+}
